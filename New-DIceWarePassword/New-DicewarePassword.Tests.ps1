220d9e4e44ba989cfd379e817fa0838d261f7717
Rob Harman <61101581+robharman@users.noreply.github.com>
2021-06-07 17:40:01 -0400

    Housekeeping

--- New-DIceWarePassword/New-DicewarePassword.Tests.ps1
+++ New-DIceWarePassword/New-DicewarePassword.Tests.ps1
@@ -4,7 +4,7 @@ describe 'New-DicewarePassword tests' {
         $SpecialCharacters  =   " `~!@#$%^&*()_+-=[]\{}|;':,./<>?""" -split ""
     }
 
-    mock Get-AuthenticodeSignature {return @{state = "Signed"}}
+    mock Get-AuthenticodeSignature { return @{ State = "Signed" } }
 
     it 'passes all default PSScriptAnalyzer rules' {
         Invoke-ScriptAnalyzer -Path "$PSScriptRoot\New-DicewarePassword.ps1" | should -BeNullOrEmpty
@@ -16,25 +16,27 @@ describe 'New-DicewarePassword tests' {
 
     it 'should return a simple password when asked' {
         $TestPassword   =   New-DiceWarePassword -Simple
-        $TestPassword.Substring(0,($TestPassword.Length -6)) -match '^[a-zA-Z]+$' | should -BeTrue
+        $TestPassword.Substring( 0,( $TestPassword.Length -6 ) ) -match '^[a-zA-Z]+$' | should -BeTrue
         $TestPassword[-6] | should -BeIn $SpecialCharacters
         { [int]$TestPassword.Substring($TestPassword.Length -5) } | should -Not -Throw
-
     }
 
     it 'should return a long enough password' {
-        (New-DiceWarePassword -NumberOfWords 5).split(' ').Count | should -BeGreaterOrEqual 5
+        ( New-DiceWarePassword -NumberOfWords 5 ).split(' ').Count | should -BeGreaterOrEqual 5
     }
 
     it 'should not be too long' {
-        (New-DiceWarePassword -NumberOfWords 3).split(' ').Count | should -BeLessOrEqual 6
+        ( New-DiceWarePassword -NumberOfWords 3 ).split(' ').Count | should -BeLessOrEqual 6
     }
 
     it 'should be complex when not simple' {
-        (New-DiceWarePassword) -match '^[a-zA-Z0-9]+$' | should -BeFalse
+        ( New-DiceWarePassword ) -match '^[a-zA-Z0-9]+$' | should -BeFalse
     }
 
-
+    it 'should not show a warning when you supress it ' {
+        $WarningPreference  =   "Stop"
+        { New-DicewarePassword -SuppressWarning } | should -not -Throw
+    }
 }
 
 describe 'New-DiceWarePassword Failure tests' {
@@ -43,7 +45,7 @@ describe 'New-DiceWarePassword Failure tests' {
     }
 
     it 'should throw when signature fails' {
-        mock Get-AuthenticodeSignature {return @{state = "NotSigned"}}
-        { New-DicewarePassword } | should -throw
+        mock Get-AuthenticodeSignature { return @{ State = "NotSigned" } }
+        { New-DicewarePassword } | should -Throw
     }
 }
